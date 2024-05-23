8e2028408b1f7222c4f9f47144d14281d7f2f12e
Muhammad Rehman <141804487+MRehman365@users.noreply.github.com>
2024-05-23 17:51:03 +0500

    services pages images update

--- src/component/services/ServiceData.js
+++ src/component/services/ServiceData.js
@@ -1,30 +1,12 @@
-import WelcomeImg from "../../assets/welcome-img.jpg";
 import { IoIosArrowRoundForward } from "react-icons/io";
-import DotNetImg from "../../assets/microsoft_net_development-removebg-preview (4).png";
-import JavaImg from "../../assets/JAVA-removebg-preview.png";
-import PhpImg from "../../assets/php_developer-removebg-preview (3).png";
-import MernImg from "../../assets/stock_development-removebg-preview (3).png";
-import ReactImg from "../../assets/REACT_NATIVE-removebg-preview.png";
-import AndroidImg from "../../assets/ANDROID-removebg-preview.png";
-import FlutterImg from "../../assets/flutter-removebg-preview.png";
-import SeoImg from "../../assets/SEO_PNG-removebg-preview.png";
-import MarketImg from "../../assets/CONTENT_MARKETING-removebg-preview (1).png";
-import SocialImg from "../../assets/social_media_marketing-removebg-preview (1).png";
-import OptiImg from "../../assets/CONSERVATION_RATE_OPTIMIZATIO-removebg-preview.png";
 import { MdOutlineConstruction } from "react-icons/md";
-import appImg from "../../assets/1715169322133_h3x5ro_2_0.jpg";
-import webImg from "../../assets/1715151392526_a932l8_2_0.jpg";
-import digImg from "../../assets/1714548656867_h82h4v_2_0.jpg";
-import applicationImg from "../../assets/1715154300452_bjeoh5_2_0.jpg";
-import websiteImg from "../../assets/web development .png";
-import digitalImg from "../../assets/OFFICE.png";
 export const ServiceData = [
   {
     url: "/application-development",
     title2: "Result Driven App Development",
     text: "Propel to new heights for App Development with Maxify Industries",
-    img: appImg,
-    img2: applicationImg,
+    img: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462406/myCloud/1715169322133_h3x5ro_2_0_iepl1u.jpg",
+    img2: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462407/myCloud/1715154300452_bjeoh5_2_0_fftxas.jpg",
     name: "Application Development",
     icons: <MdOutlineConstruction />,
     title: "Empowering Excellence with The Right App Development Company",
@@ -37,25 +19,25 @@ export const ServiceData = [
       "Explore our wide range of technologies used for app development ",
     technologyData: [
       {
-        technologyImg: ReactImg,
+        technologyImg: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462487/myCloud/REACT_NATIVE-removebg-preview_voblom.png",
         technologyName: "React Native",
         technologyAbout:
           "As a robust react native app development company, our team designs and develops frameworks for your applications to deliver tailor-made and highly competitive applications to align with your custom needs. To understand your product vision and find the best possibilities and solutions aligned with your business goals.",
       },
       {
-        technologyImg: AndroidImg,
+        technologyImg: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462411/myCloud/ANDROID-removebg-preview_evqyj2.png",
         technologyName: "Android",
         technologyAbout:
           "As a leading android app developer company, we offer a range of services that are fully capable of supporting your needs and ideating your vision into reality. Our services are a testament to various android platforms and devices, that help us delve deep into the work world and explore all possible outcomes to witness more recognition.",
       },
       {
-        technologyImg: FlutterImg,
+        technologyImg: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462442/myCloud/flutter-removebg-preview_c1lu2d.png",
         technologyName: "Flutter",
         technologyAbout:
           " We craft cost-effective and efficient applications that strengthens your business across various dimensions. Our developers take result-drive approach to develop applications bringing out the best for your business and fulfilling commitments by optimizing apps in a perfect framework. ",
       },
       {
-        technologyImg: JavaImg,
+        technologyImg: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462460/myCloud/JAVA-removebg-preview_nn4n43.png",
         technologyName: "Java",
         technologyAbout:
           "As a java application development company, we have experience in building multi-platform desktop applications with help of different tools and frameworks. We ensure to have a smooth workflow of application so it can perform well considering the specific needs of your business. ",
@@ -64,8 +46,8 @@ export const ServiceData = [
   },
   {
     url: "/web-development",
-    img2: websiteImg,
-    img: webImg,
+    img2: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462510/myCloud/web_development_wmnybw.png",
+    img:  "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462405/myCloud/1715151392526_a932l8_2_0_wax4jn.jpg",
     title2: "Maxifying Your Web Presence",
     text: "Establish Your Business Presence with the Best Web Development Company",
     name: "Web development",
@@ -81,25 +63,25 @@ export const ServiceData = [
       "We are experts in building digital platforms, developing new applications and modernizing legacy business processes",
     technologyData: [
       {
-        technologyImg: DotNetImg,
+        technologyImg: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462469/myCloud/microsoft_net_development-removebg-preview_4_tejmlq.png",
         technologyName: "Full Stack Development",
         technologyAbout:
           "To deliver better quality work, we provide end-to-end full stack development services using various technologies such as HTML, JavaScript, CSS, Django, Noe.js, Laravel, MySQL, Oracle and many more. It helps you get a seamless experience of the website and better data security and accessibility bringing unmatched value to your business.",
       },
       {
-        technologyImg: JavaImg,
+        technologyImg: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462460/myCloud/JAVA-removebg-preview_nn4n43.png",
         technologyName: "Java",
         technologyAbout:
           " As a leading Java development company, we provide tailored solutions to suite your business needs. Maxify Industries specializes in B2C and B2B web and application development. We aim at building performance-oriented, secure and reliable web applications that enhance the value of business and user credibility.",
       },
       {
-        technologyImg: PhpImg,
+        technologyImg: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462480/myCloud/php_developer-removebg-preview_3_p7mlsk.png",
         technologyName: " PHP Development",
         technologyAbout:
           "To enhance your digital presence, we have the best Php Development services offered. We design and integrate effective custom content management systems that enhances the user experience to ensure the effective business-customer relationship.  ",
       },
       {
-        technologyImg: MernImg,
+        technologyImg: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462501/myCloud/stock_development-removebg-preview_3_otmo2l.png",
         technologyName: " MERN Stack Development",
         technologyAbout:
           " We provide latest cutting edge solutions with MERN Stack development for websites and app development. We use Magento, Flutter, Firebase, React-Native, Django, MeteorJS Frameworks for development. We have highly talented professionals who will turn your dreams into reality with the extravagant dedication they put in work. ",
@@ -108,8 +90,8 @@ export const ServiceData = [
   },
   {
     url: "/digital-marketing",
-    img: digImg,
-    img2: digitalImg,
+    img: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462399/myCloud/1714548656867_h82h4v_2_0_fl6qnd.jpg",
+    img2: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462476/myCloud/OFFICE_zy0noi.png",
     title2: "Connecting Your Business World Wide",
     text: "Connect Your Business with Your Audience With Our Digital Marketing Services",
     name: "Digital Marketing",
@@ -125,70 +107,70 @@ export const ServiceData = [
       "We are experts in building digital platforms, developing new applications and modernizing legacy business processes",
     technologyData: [
       {
-        technologyImg: SeoImg,
+        technologyImg: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462493/myCloud/SEO_PNG-removebg-preview_nqycgp.png",
         technologyName: "SEO",
         technologyAbout:
           "Amplify your market reach and brand building with thriving SEO strategies that help you stand out from the crowd by optimizing your website, ensuring brand consistency, getting positive response, geo-modified service pages ensuring our business secure high rankings worldwide as per the domain expertise.",
       },
       {
-        technologyImg: SocialImg,
+        technologyImg: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462501/myCloud/social_media_marketing-removebg-preview_1_snrs5c.png",
         technologyName: "Social Media Marketing",
         technologyAbout:
           " Expand your market reach and audience with various social media platforms. We build social media campaigns and paid advertising solutions based on the target audience, region, goals of your business which performs better than other competitors and evaluates your customers’ online behaviour. ",
       },
       {
-        technologyImg: OptiImg,
+        technologyImg: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462427/myCloud/CONSERVATION_RATE_OPTIMIZATIO-removebg-preview_oclodm.png",
         technologyName: " Conversion rate Optimization",
         technologyAbout:
           "   Increased conversion rates by optimizing your website to push page visitors into increase conversion rates. We optimize websites as per the devices used creating responsive landing pages, easily navigating throughout the website, and quick responses to promote better conversions. ",
       },
       {
-        technologyImg: MarketImg,
+        technologyImg: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462426/myCloud/CONTENT_MARKETING-removebg-preview_1_oykhlm.png",
         technologyName: " Content Marketing",
         technologyAbout:
           " Build lasting relationships with the desired audience and increase in brand’s visibility. We entrust your website to have the right content we use high-performing keywords which helps to rank your website easily and structure content so it can promote easily reading and convince people to visit your website frequently. ",
       },
     ],
   },
-  {
-    url: "/graphic-design",
-    img: WelcomeImg,
-    name: "Graphic Designing",
-    icons: <MdOutlineConstruction />,
-    title: "Digitalizing legacy systems with custom Graphic Designing services",
-    description:
-      "Maxify Web Solution, a leading application development company with a client base in Singapore, London, New York, San Francisco, Dubai & all around the globe, has comprehensive experience in moving legacy systems to modernized and advanced platforms through its web application development services. Backed by strong knowledge of mainstream technologies, our dedicated app developers offer cross-platform application development solutions and build responsive, robust and scalable apps in line with your existing IT infrastructure. With our head office in Singapore, we have 8 offices globally in the US, UK, Middle East and APAC regions.",
-    description2:
-      "Enterprise app development services to help you gain first-mover advantage",
-    technologyTitle: "Specialization in multiple app development platform",
-    technologydescription:
-      "We are experts in building digital platforms, developing new applications and modernizing legacy business processes",
+  // {
+  //   url: "/graphic-design",
+  //   img: "https://res.cloudinary.com/djkkjx9ry/image/upload/v1716462399/myCloud/1714548656867_h82h4v_2_0_fl6qnd.jpg",
+  //   name: "Graphic Designing",
+  //   icons: <MdOutlineConstruction />,
+  //   title: "Digitalizing legacy systems with custom Graphic Designing services",
+  //   description:
+  //     "Maxify Web Solution, a leading application development company with a client base in Singapore, London, New York, San Francisco, Dubai & all around the globe, has comprehensive experience in moving legacy systems to modernized and advanced platforms through its web application development services. Backed by strong knowledge of mainstream technologies, our dedicated app developers offer cross-platform application development solutions and build responsive, robust and scalable apps in line with your existing IT infrastructure. With our head office in Singapore, we have 8 offices globally in the US, UK, Middle East and APAC regions.",
+  //   description2:
+  //     "Enterprise app development services to help you gain first-mover advantage",
+  //   technologyTitle: "Specialization in multiple app development platform",
+  //   technologydescription:
+  //     "We are experts in building digital platforms, developing new applications and modernizing legacy business processes",
 
-    technologyData: [
-      {
-        technologyImg: DotNetImg,
-        technologyName: "Microsoft .NET Development",
-        technologyAbout:
-          "Our ASP.NET developers follow efficient project management processes to develop feature-rich .NET portals and apps.",
-      },
-      {
-        technologyImg: JavaImg,
-        technologyName: "Enterprise Java Development",
-        technologyAbout:
-          "  Maxify Web Solution has vast knowledge of the latest Java platforms, including J2EE (Enterprise Edition) and J2SE (Standard Edition).",
-      },
-      {
-        technologyImg: PhpImg,
-        technologyName: " PHP Development",
-        technologyAbout:
-          "        Our PHP developers are well-versed with the latest PHP development tools and frameworks which enable them to cater to the requirements of global businesses. ",
-      },
-      {
-        technologyImg: MernImg,
-        technologyName: " MEAN Stack Development",
-        technologyAbout:
-          " Maxify Web Solution possesses a deep understanding of MEAN - a JavaScript-based technology platform, and creates versatile and feature-rich mobile and web applications. ",
-      },
-    ],
-  },
+  //   technologyData: [
+  //     {
+  //       technologyImg: DotNetImg,
+  //       technologyName: "Microsoft .NET Development",
+  //       technologyAbout:
+  //         "Our ASP.NET developers follow efficient project management processes to develop feature-rich .NET portals and apps.",
+  //     },
+  //     {
+  //       technologyImg: JavaImg,
+  //       technologyName: "Enterprise Java Development",
+  //       technologyAbout:
+  //         "  Maxify Web Solution has vast knowledge of the latest Java platforms, including J2EE (Enterprise Edition) and J2SE (Standard Edition).",
+  //     },
+  //     {
+  //       technologyImg: PhpImg,
+  //       technologyName: " PHP Development",
+  //       technologyAbout:
+  //         "        Our PHP developers are well-versed with the latest PHP development tools and frameworks which enable them to cater to the requirements of global businesses. ",
+  //     },
+  //     {
+  //       technologyImg: MernImg,
+  //       technologyName: " MEAN Stack Development",
+  //       technologyAbout:
+  //         " Maxify Web Solution possesses a deep understanding of MEAN - a JavaScript-based technology platform, and creates versatile and feature-rich mobile and web applications. ",
+  //     },
+  //   ],
+  // },
 ];
